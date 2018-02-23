899325df0cb570ce11b932e6c2971efaf2d61aaf
James McDowell <jamesnmcdowell@gmail.com>
2018-02-22 22:44:08 -0500

    add gridbox-based lightbox to gallery

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var imgArray = [
+let imgArray = [
   {
     img: "https://images.pexels.com/photos/545964/pexels-photo-545964.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb",
     likes: 2,
@@ -47,15 +47,139 @@ var imgArray = [
 ];
 
 function populateGallery() {
-  var gallery = document.querySelector(".image-gallery");
+  let gallery = document.querySelector(".image-gallery");
   for (i = 0; i < imgArray.length; i++) {
-    var imageWrapper = document.createElement('a');
-    var image = document.createElement('img');
+    let imageWrapper = document.createElement('a');
+    let image = document.createElement('img');
     image.setAttribute('src', imgArray[i].img);
     imageWrapper.appendChild(image);
     imageWrapper.classList.add("gallery-item");
+    imageWrapper.classList.add(i);
+    gallery.appendChild(imageWrapper);
+  }
+};
+
+function populateLightbox() {
+  let gallery = document.querySelector(".images-container");
+  for (i = 0; i < imgArray.length; i++) {
+    let imageWrapper = document.createElement('div');
+    let image = document.createElement('img');
+    image.setAttribute('src', imgArray[i].img);
+    imageWrapper.appendChild(image);
+    imageWrapper.classList.add("image");
+    imageWrapper.classList.add(`img${i}`);
     gallery.appendChild(imageWrapper);
   }
 };
 
 populateGallery();
+populateLightbox();
+
+
+ let prevImgBtn = document.querySelector('.control-left');
+ let nextImgBtn = document.querySelector('.control-right');
+ let allImages = Array.from(document.querySelectorAll('.images-container > div'));
+ let allImagesLen = allImages.length;
+
+function movePrevious(){
+  let selectedImg = document.querySelector('.images-container > .selected');
+   let selectedIdx = allImages.findIndex(el => el.isEqualNode(selectedImg));
+   let nextIdx = (selectedIdx - 1) < 0 ? allImagesLen - 1 : selectedIdx - 1;
+   if (selectedImg) {
+     selectedImg.classList.remove('selected');
+     allImages[nextIdx].classList.add('selected');
+   }
+}
+
+
+ prevImgBtn.addEventListener('click', movePrevious);
+
+ nextImgBtn.addEventListener('click', () => {
+   let selectedImg = document.querySelector('.images-container > .selected');
+   let selectedIdx = allImages.findIndex(el => el.isEqualNode(selectedImg));
+   let nextIdx = (selectedIdx + 1) % allImagesLen;
+   if (selectedImg) {
+     selectedImg.classList.remove('selected');
+     allImages[nextIdx].classList.add('selected');
+   }
+ });
+
+
+
+
+let current;
+let imgs = document.querySelectorAll('.image-gallery .gallery-item');
+let lightbox = document.querySelector('.lightbox-overlay');
+
+
+imgs.forEach(img => img.addEventListener('click', imgClick));
+
+function imgClick(e) {
+  let current = e.target.parentElement.classList[1];
+  let classSelector = `.images-container .image.img${String(current)}`;
+  let imageSelected = document.querySelector(classSelector);
+  imageSelected.classList.add('selected');
+  lightbox.classList.toggle('open');
+
+}
+
+let lightboxImage = document.querySelector('.main-grid');
+
+lightbox.addEventListener('click', function(event) {
+  let isClickInside = lightboxImage.contains(event.target);
+  if (!isClickInside && lightbox.classList.contains('open') ) {
+    console.log("outside");
+    lightbox.classList.toggle('open');
+    let selectedImg = document.querySelector('.images-container .selected');
+    if (selectedImg.classList.contains("selected")) {
+      selectedImg.classList.remove("selected");
+    }
+  }
+});
+
+document.onkeydown = function(evt) {
+  if (lightbox.classList.contains('open') ) {
+    evt = evt || window.event;
+    let isEscape = false;
+    if ("key" in evt) {
+        isEscape = (evt.key == "Escape" || evt.key == "Esc");
+    } else {
+        isEscape = (evt.keyCode == 27);
+    }
+    if (isEscape) {
+        lightbox.classList.toggle('open');
+        let selectedImg = document.querySelector('.images-container .selected');
+        if (selectedImg.classList.contains("selected")) {
+          selectedImg.classList.remove("selected");
+       }
+    }
+    
+    let isLeft = false;
+    if ("key" in evt) {
+        isLeft = (evt.key == "Left arrow" );
+    } else {
+        isLeft = (evt.keyCode == 37);
+    }
+    if (isLeft) {
+      movePrevious();
+        
+    }
+    
+    let isRight = false;
+    if ("key" in evt) {
+        isRight = (evt.key == "Right arrow");
+    } else {
+        isRight = (evt.keyCode == 39);
+    }
+    if (isRight) {
+     
+    }
+    
+
+  }
+};
+
+
+
+
+
