785c7153ced61d6d8d754521fff377c9c40e4ea5
James McDowell <jamesnmcdowell@gmail.com>
2018-02-26 17:12:01 -0500

    continue refactoring JS

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ let imgArray = [
     comments: 3
   },
   {
-    src: "https://images.pexels.com/photos/144251/yosemite-national-park-landscape-california-144251.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb",
+    src: "https://images.pexels.com/photos/161784/landscape-thor-s-hammer-bryce-canyon-national-park-161784.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
     likes: 2,
     comments: 3
   },
@@ -40,12 +40,11 @@ let imgArray = [
     comments: 3
   },
   {
-    src: "https://images.pexels.com/photos/459028/pexels-photo-459028.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb",
+    src: "https://images.pexels.com/photos/221148/pexels-photo-221148.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
     likes: 2,
     comments: 3
   }
 ];
-
 
 let allImagesLen = imgArray.length;
 let gallery = document.querySelector(".image-gallery");
@@ -53,8 +52,11 @@ let lightbox = document.querySelector('.lightbox-overlay');
 let lightboxFocus = document.querySelector('.lightbox-wrapper img');
 let prevImgBtn = document.querySelector('.control-left');
 let nextImgBtn = document.querySelector('.control-right');
+let lightboxWrapper = document.querySelector('.lightbox-wrapper');
+let mainGrid = document.querySelector('.main-grid');
+let imgCointainer = document.querySelector('.main-grid .image');
 
-imgArray.forEach(function (image, i) {
+imgArray.forEach( (image, i) => {
   let imageWrapper = document.createElement('a');
   let imageTag = document.createElement('img');
   imageTag.dataset.imgIndex = i;
@@ -70,63 +72,40 @@ imgArray.forEach(function (image, i) {
   })
 });
 
-prevImgBtn.addEventListener('click', () => {
+let movePrev = () => {
   let currentIndex = lightboxFocus.dataset.imgIndex;
   let prevIdx = (lightboxFocus.dataset.imgIndex - 1) < 0 ? allImagesLen - 1 : lightboxFocus.dataset.imgIndex - 1;
   lightboxFocus.setAttribute('src', imgArray[prevIdx].src);
   lightboxFocus.dataset.imgIndex = prevIdx;
-});
+  };
 
-nextImgBtn.addEventListener('click', () => {
+let moveNext = () => {
   let currentIndex = lightboxFocus.dataset.imgIndex;
   let nextIdx = (currentIndex + 1) % allImagesLen;
   lightboxFocus.setAttribute('src', imgArray[nextIdx].src);
   lightboxFocus.dataset.imgIndex = nextIdx;
-});
+  };
 
-lightbox.addEventListener('click', function (event) {
-  let isClickInside = lightboxFocus.contains(event.target);
-  let isClickInside2 = prevImgBtn.contains(event.target);
-  let isClickInside3 = nextImgBtn.contains(event.target);
-  //check if clicked outside of image lightbox
-  if (!isClickInside && lightbox.classList.contains('open') && !isClickInside2 && !isClickInside3) {
+prevImgBtn.addEventListener('click', movePrev);
+nextImgBtn.addEventListener('click', moveNext);
+
+lightbox.addEventListener('click', (e) => {
+  let isClickInside1 = lightboxWrapper.contains(e.target);
+  let isClickInside2 = mainGrid.contains(e.target);
+  let isClickInside3 = imgCointainer.contains(e.target);
+  if (e.target === lightboxWrapper | e.target === mainGrid | e.target === imgCointainer ) {
     lightbox.classList.toggle('open');
-    let selectedImg = document.querySelector('.images-container .selected');
   }
 });
 
-document.onkeydown = function (evt) {
+document.onkeydown = (e) => {
   if (lightbox.classList.contains('open')) {
-    evt = evt || window.event;
-    let isEscape = false;
-    if ("key" in evt) {
-      isEscape = (evt.key == "Escape" || evt.key == "Esc");
-    } else {
-      isEscape = (evt.keyCode == 27);
-    }
-    if (isEscape) {
-      lightbox.classList.toggle('open');
-      let selectedImg = document.querySelector('.images-container .selected');
-    }
-
-    let isLeft = false;
-    if ("key" in evt) {
-      isLeft = (evt.key == "Left arrow");
-    } else {
-      isLeft = (evt.keyCode == 37);
-    }
-    if (isLeft) {
-      movePrevious();
-    }
-
-    let isRight = false;
-    if ("key" in evt) {
-      isRight = (evt.key == "Right arrow");
-    } else {
-      isRight = (evt.keyCode == 39);
-    }
-    if (isRight) {
-
-    }
+    e = e || window.event;
+    let isEscape = "key" in e ? e.key == "Escape" || e.key == "Esc" : e.keyCode == 27
+    if (isEscape) lightbox.classList.toggle('open');
+    let isLeft = "key" in e ? e.key == "ArrowLeft" : e.keyCode == 37;
+    if (isLeft) movePrev();
+    let isRight = "key" in e ? e.key == "ArrowRight" : e.keyCode == 39;
+    if (isRight) moveNext();
   }
 };
